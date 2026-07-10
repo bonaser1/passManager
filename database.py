28d6b1e189d44ceb599f2bbd24d528ba12bc203e
root <root@DESKTOP-JPQVEO4.localdomain>
2026-07-10 23:35:59 +0300

    done v1

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 import sqlite3
+from pathlib import Path
 
-DB_NAME = 'passwordsDataBase.db'
+BASE_DIR = Path(__file__).parent
+DB_NAME = BASE_DIR / 'passwordsDataBase.db'
 
 
 
@@ -21,38 +23,39 @@ def get_choice():
 def search_passwords():
     with sqlite3.connect(DB_NAME) as connect:
         cursor = connect.cursor()
-    #checking if the db is empty
-    cursor.execute("SELECT 1 FROM passwords LIMIT 1")
-    result = cursor.fetchone()
-    if result is None:
-        print('No saved passwords')
-    else:
-        #Searching the db for stored passwords
-        app, user = get_app_searching()
-        cursor.execute("SELECT password FROM passwords WHERE app_name=? AND user_name=?", (app, user))
-        search_res = cursor.fetchone()
-        if search_res:
-            print('passwords found: ')
-            print(f'    {search_res[0]}')
+
+        #checking if the db is empty
+        cursor.execute("SELECT 1 FROM passwords LIMIT 1")
+        result = cursor.fetchone()
+        if not result:
+            print('\n>>> No saved passwords')
         else:
-            print('No data found!!')
+            #Searching the db for stored passwords
+            app, user = get_app_searching()
+            cursor.execute("SELECT password FROM passwords WHERE app_name=? AND user_name=?", (app, user))
+            search_res = cursor.fetchone()
+            if search_res:
+                print('passwords found: ')
+                print(f'    {search_res[0]}')
+            else:
+                print('\n>>> No data found!!')
 
 def add_password():
     with sqlite3.connect(DB_NAME) as connect:
         cursor = connect.cursor()
-    app, user, password = get_app()
-    cursor.execute("INSERT INTO passwords(app_name, user_name, password) VALUES (?, ?, ?)", (app, user, password))
-    print(f'Done adding the new password.')
+        app, user, password = get_app()
+        cursor.execute("INSERT INTO passwords(app_name, user_name, password) VALUES (?, ?, ?)", (app, user, password))
+        print(f'\n>>> Done adding the new password.')
 
 def delete_password():
     with sqlite3.connect(DB_NAME) as connect:
         cursor = connect.cursor()
-    app, user, password = get_app()
-    question = input('are you sure you want to delete that password? [y/n]: ')
-    if question == 'y':
-        cursor.execute("DELETE FROM passwords WHERE app_name=? AND user_name=? AND password=?", (app, user, password))
-        print(f'the password has been deleted.')
-    elif question == 'n':
-        pass
-    else:
-        print('Enter a valid value!!')
+        app, user, password = get_app()
+        question = input('are you sure you want to delete that password? [y/n]: ')
+        if question == 'y':
+            cursor.execute("DELETE FROM passwords WHERE app_name=? AND user_name=? AND password=?", (app, user, password))
+            print(f'\n>>> the password has been deleted.')
+        elif question == 'n':
+            pass
+        else:
+            print('\n>>> Enter a valid value!!')
--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 def main():
+    print('\n')
     print('Password Manager App v0.1\n')
     print('         [1] Looking for a password?')
     print('         [2] Saving a password?')
